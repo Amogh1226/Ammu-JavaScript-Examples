fc043f76680fcffbe3512543b5a9304ac4814485
Amogh <amogh.jonnalagadda@gmail.com>
2021-02-17 16:16:05 +0800

    Ammu-

---
+++ four.js
@@ -0,0 +1,60 @@
+''var city = " amstErdaM"
+
+var city = "amstErdaM"
+undefined
+city.toUpperCase()
+"AMSTERDAM"
+city.toLowerCase()
+"amsterdam"
+
+"Amsterdam"
+
+var city = "amstErdaM"
+undefined
+city[0]
+"a"
+city[1]
+"m"
+city.length
+9
+
+var city = "    London  "
+undefined
+city.length
+12
+
+var city = "    London  "
+city.trim()
+
+var city = "    London  "
+city.trim()
+"London"
+
+var email = "Amogh.jonnalagadda@gamil.com"
+var emailid = "amogh.jonnalagadda@gamil.com"
+
+var email = "Amogh.jonnalagadda@gamil.com"
+var emailid = "amogh.jonnalagadda@gamil.com"
+undefined
+email == emailid
+false
+email.toUpperCase() == emailid.toUpperCase()
+true
+
+var email = "Ahanda205"
+undefined
+email.charAt(0)
+"A"
+email[0]
+"A"
+
+var email = "Ahanda205"
+undefined
+email.slice(1)
+"handa205"
+email.slice(2)
+"anda205"
+email.slice(1,3)
+"ha"
+email.slice(1,4)
+"han"
